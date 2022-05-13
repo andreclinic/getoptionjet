b0b2f6c371afe063ac94fb118f2916edbe3679ef
André Corrêa <57374879+andreclinic@users.noreply.github.com>
2022-05-12 21:50:55 -0300

    Create function.php

---
+++ function.php
@@ -0,0 +1,6 @@
+	
+	/** Sessão 01 - Títulos da Banner **/
+		$titulo_sessao_01 = jet_engine()->listings->data->get_option('config-sistema::titulo-sessao-banner-01');
+		$subtitulo_sessao_01 = jet_engine()->listings->data->get_option('config-sistema::subtitulo-sessao-banner-01');
+		$texto_sessao_01 = jet_engine()->listings->data->get_option('config-sistema::texto-sessao-banner-01');
+		$texto_sessao_01t = str_replace(PHP_EOL, '', $texto_sessao_01);
